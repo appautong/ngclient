5c6b2b084a092357b608812ca91dba05a0283306
appautong <appautong@gmail.com>
2021-01-29 18:07:46 +0800

    autodraw test (temp)
    appauto context test

--- android/app/src/main/java/cc/appauto/ngclient/MainActivity.kt
+++ android/app/src/main/java/cc/appauto/ngclient/MainActivity.kt
@@ -1,30 +1,21 @@
 package cc.appauto.ngclient
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.graphics.PixelFormat
+import android.app.Activity
+import android.app.AlertDialog
 import android.graphics.Rect
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.provider.Settings
-import android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION
 import android.util.Log
-import android.view.Gravity
 import android.view.KeyEvent
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.loader.content.AsyncTaskLoader
+import cc.appauto.lib.ng.AppAutoContext
+import cc.appauto.lib.ng.checkPermissions
+import cc.appauto.lib.ng.randomInt
 import com.facebook.react.PackageList
 import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactRootView
 import com.facebook.react.common.LifecycleState
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
-import cc.appauto.lib.ng.AppAutoContext
-import cc.appauto.lib.ng.UIAnnotation
-import cc.appauto.lib.ng.getAppBound
-import cc.appauto.lib.ng.randomInt
 
 private const val TAG = "ngclient"
 
@@ -62,6 +53,17 @@ class MainActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
     override fun onResume() {
         super.onResume()
         mReactInstanceManager?.onHostResume(this, this)
+        val builder = AlertDialog.Builder(this)
+
+        val diag = builder.setTitle(cc.appauto.lib.R.string.appauto_check_permission)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setCancelable(false)
+                .create()
+        diag.show()
+        AppAutoContext.workHandler.postDelayed({
+            diag.dismiss()
+            AppAutoContext.checkPermissions(this)
+        }, 2000)
     }
 
     override fun onDestroy() {
@@ -80,34 +82,9 @@ class MainActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
             moveTaskToBack(true)
     }
 
-    fun showOverylay() {
-        val overlay = Settings.canDrawOverlays(this.applicationContext)
-        Log.i(TAG, "can draw overlay: $overlay")
-        if (!overlay) {
-            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-            this.startActivity(intent)
-            return
-        }
-        val wm = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val params = WindowManager.LayoutParams()
-        params.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
-        params.gravity = Gravity.LEFT or Gravity.TOP
-        params.format = PixelFormat.RGBA_8888
-        params.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
-        params.width = randomInt(100, 600)
-        params.height = randomInt(100, 1200)
-        params.x = randomInt(0, 200)
-        params.y = randomInt(0, 200)
-        val anno = UIAnnotation(this.applicationContext, Rect(params.x, params.y, params.width+params.x, params.height+params.y))
-        wm.addView(anno, params)
-        anno.invalidate()
-    }
-
     override fun onKeyUp(keyCode: Int, event: KeyEvent?): Boolean {
         if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-            showOverylay()
             mReactInstanceManager?.showDevOptionsDialog()
-            return true
         }
         return super.onKeyUp(keyCode, event)
     }
